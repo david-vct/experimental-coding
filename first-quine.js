4f0bef1f417a8343d4a5b212a4a5da1608fee353
David Vicente <davidvicento@gmail.com>
2026-07-21 00:26:42 +0200

    feat(quine): add my first quine implementation in js

---
+++ first-quine.js
@@ -0,0 +1,18 @@
+// This program prints it self
+(() => {
+    const _ = unescape("%27")
+    const $ = [
+        '// This program prints it self',
+        '(() => {',
+        '    const _ = unescape("%27")',
+        '    const $ = [',
+        '    ]',
+        '    for (let i=0; i<4; i++) console.log($[i])',
+        '    for (let i=0; i<9; i++) console.log(`        ${_}${$[i]}${_},`)',
+        '    for (let i=4; i<9; i++) console.log($[i])',
+        '})()',
+    ]
+    for (let i=0; i<4; i++) console.log($[i])
+    for (let i=0; i<9; i++) console.log(`        ${_}${$[i]}${_},`)
+    for (let i=4; i<9; i++) console.log($[i])
+})()
