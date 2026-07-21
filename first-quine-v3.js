b3fa17f8dd03b7c4a56d721ef34d779f538ef129
David Vicente <davidvicento@gmail.com>
2026-07-21 03:03:18 +0200

    feat(quine): add a 256 byte quine

---
+++ first-quine-v3.js
@@ -0,0 +1,7 @@
+// This program prints itself in exactly 256 bytes.
+_=unescape("%0A"),$= [
+  '// This program prints itself in exactly 256 bytes.',
+  '_=unescape("%0A"),$=',
+  ':Da/1D_vct>was+here<3',
+  ';console.log($[0]+_+$[1],$,$[3])'
+] ;console.log($[0]+_+$[1],$,$[3])
