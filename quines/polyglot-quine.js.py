ccb4fc78475e045b67eb18038d472cc279d27ac6
David Vicente <davidvicento@gmail.com>
2026-08-14 16:45:40 +0200

    feat(quines): add partial js and py polyglot quine

--- quines/polyglot-quine.js.py
+++ quines/polyglot-quine.js.py
@@ -1,4 +1,2 @@
+lambda:eval("s=\"console.log('lambda:eval('+JSON.stringify('s='+JSON.stringify(s)+';eval(s)')+')')\";eval(s)")
 1//(lambda:exec(s:="print('1//(lambda:exec(s:=%r) or 1)()'%s)") or 1)()
-lambda:eval("(s=_=>`lambda:exec(s=${s})`)()")
-
-1//1# Pour js utilser console log et JSON stringify
